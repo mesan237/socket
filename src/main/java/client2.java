0e5ee4e861b2c5c2a4ba0bc0d2829848e7af203a
Abed <mesan237@users.noreply.github.com>
2022-05-08 19:23:49 +0100

    modif de client2.java

--- src/main/java/client2.java
+++ src/main/java/client2.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-
+  
 public class Client {
     public Client(String host, int port) throws  Exception{
         Socket socket = new Socket(host, port);
@@ -16,7 +16,11 @@ public class Client {
 
         socket.close();
         System.out.println("The socket is closed");
-    }
+    
+
+	
+	
+}
 
     public static void main(String[] args) {
         try {
